0ba25c80890d1f9d9cf8d5032bf7efa1006d0277
Matt Teixeira <matt1teixeira@gmail.com>
2022-11-25 16:15:18 -0500

    Init commit

---
+++ server/controllers/userControllers.js
@@ -0,0 +1,7 @@
+const getUsers = async (req, res) => {
+  res.send("Getting all users");
+};
+
+module.exports = {
+  getUsers,
+};
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const db = require("./config/connection");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log("Listning at port: " + PORT);
+  });
+});
